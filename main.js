f4db92d9a1cfb31d1ece51e7149d72c8e46cb274
rareramos <rareramos1126@gmail.com>
2020-06-09 01:59:04 +0300

    update animation by adding headline, and fixing feedback

---
+++ main.js
@@ -0,0 +1,39 @@
+
+function isPortrait() {
+  return window.innerHeight > window.innerWidth;
+}
+
+const tween = new TimelineLite();
+const startElementPosition = isPortrait() ? "-125vw" : "-85vw";
+
+tween
+  .from(".cartridge-hint .circle", 2, { autoAlpha: 0 })
+  .from(".cartridge-hint .content", 1, { autoAlpha: 0 })
+  .to(".hint.cartridge-hint", 1, { autoAlpha: 0 }, "+=2")
+  .from(".filter", 6, { "margin-top": startElementPosition })
+  .from(".silicone-hose-hint .circle", 2, { autoAlpha: 0 })
+  .from(".silicone-hose-hint .content", 1, { autoAlpha: 0 })
+  .to(".hint.silicone-hose-hint", 1, { autoAlpha: 0 }, "+=2")
+  .from(".ball", 6, { "margin-top": startElementPosition })
+  .set(".ball", { attr: { src:"images/gold-ball-cutted.png" } })
+  .to(".ball", 0, { "z-index": "10" })
+  .from(".ball-hint .circle", 2, { autoAlpha: 0 })
+  .from(".ball-hint .content", 1, { autoAlpha: 0 })
+  .to(".hint.ball-hint", 1, { autoAlpha: 0 }, "+=2")
+  .from(".faucet", 3, { "margin-top": startElementPosition })
+  .set(".faucet", { attr: { src:"images/faucet-cutted.png" } }, "-=0.3")
+  .to(".faucet", 0, { "z-index": "11" }, "+=0.3")
+  .from(".spout-hint .circle", 2, { autoAlpha: 0 })
+  .from(".spout-hint .content", 1, { autoAlpha: 0 })
+  .to(".hint.spout-hint", 1, { autoAlpha: 0 }, "+=2");
+
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: '#app',
+  triggerHook: 0,
+  duration: 7000,
+})
+  .setTween(tween)
+  .setPin('#app')
+  .addTo(controller);
